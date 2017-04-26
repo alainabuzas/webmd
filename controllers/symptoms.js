1ae40c6054ec75158fe440f3d4c35056bf3d6c2b
alainabuzas <alaina.buzas@gmail.com>
2017-04-26 09:06:55 -0700

    webmd in class work

---
+++ controllers/symptoms.js
@@ -0,0 +1,24 @@
+var express = require('express');
+var db = require('../models');
+var router = express.Router();
+
+router.get("/", function(req, res) {
+    db.symptom.findAll().then(function(symptoms) {
+        res.render("symptoms/all");
+    });
+});
+
+router.post("/", function(req, res) {
+    db.symptom.create(req.body).then(function(sympton) {
+        res.redirect("/symptoms");
+    }).catch(function(error) {
+        console.log("error occured", error);
+        res.send("ERROR");
+    });
+});
+
+router.get("/add", function(req, res) {
+    res.render("symptoms/add");
+});
+
+module.exports = router;
